5881e8a33f89d129200f5f4843caf3b939395a0f
Gokula Krishna <gokula@binary.com>
2018-04-16 16:56:58 +0800

    Deposit virtual template

---
+++ src/javascript/app_2/pages/cashier/topup/topup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TopupVirtual extends React.PureComponent {
+	render() {
+		return (<div><h1>Hello</h1></div>);
+	} 
+}
+
+export default TopupVirtual; 
--- src/javascript/app_2/routes.js
+++ src/javascript/app_2/routes.js
@@ -7,11 +7,13 @@ import { localize } from '../_common/localize';
 
 import TradeApp  from './pages/trading/trade_app.jsx';
 import Statement from './pages/statement/statement.jsx';
+import TopupVirtual from './pages/cashier/topup/topup.jsx';
 
 
 const routes = [
     { path: '/',          component: TradeApp, exact: true },
     { path: '/statement', component: Statement, is_authenticated: true },
+    { path: '/cashier/virtual/topup', component: TopupVirtual, is_authenticated: true },
 ];
 
 const RouteWithSubRoutes = route => (
