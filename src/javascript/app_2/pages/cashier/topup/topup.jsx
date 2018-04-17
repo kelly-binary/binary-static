7a938b3b2251bb3b6ce102fe645218085a5f01d4
Gokula Krishna <gokula@binary.com>
2018-04-17 17:36:44 +0800

    UI prototype

--- src/javascript/app_2/pages/cashier/topup/topup.jsx
+++ src/javascript/app_2/pages/cashier/topup/topup.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 class TopupVirtual extends React.PureComponent {
 	render() {
-		return (<div><h1>Hello</h1></div>);
+		return (<div>
+                            <h1>Top up Virtual Account</h1>
+                            <p>Congratulations, USD 10,000.00 has been credited into your virtual account</p>
+                            <div>
+                                <button>VIEW UPDATED STATEMENT</button>
+                                <button>TRADE SMART TRADER NOW</button>
+                            </div>
+                        </div>);
 	} 
 }
 
