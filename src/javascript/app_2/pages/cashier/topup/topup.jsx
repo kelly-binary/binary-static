8db012a8914fa17dda71045ae313ca66432f0fa9
Gokula Krishna <gokula@binary.com>
2018-04-18 17:04:19 +0800

    Virtual topup complete UI

--- src/javascript/app_2/pages/cashier/topup/topup.jsx
+++ src/javascript/app_2/pages/cashier/topup/topup.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import Button from '../../../components/form/button.jsx';
 
 class TopupVirtual extends React.PureComponent {
 	render() {
-		return (<div>
-                            <h1>Top up Virtual Account</h1>
-                            <p>Congratulations, USD 10,000.00 has been credited into your virtual account</p>
-                            <div>
-                                <button>VIEW UPDATED STATEMENT</button>
-                                <button>TRADE SMART TRADER NOW</button>
-                            </div>
+		return (<div id="topup">
+				<h1>Top up Virtual Account</h1>
+				<p>Congratulations, USD 10,000.00 has been credited into your virtual account</p>
+				<div>
+					<span class="topup-tile">
+						<img src="../images/trading_app/cashier/ic_virtualmoney_normal.svg" width="96" height="96" />
+						<p>USD $10,000.00</p>
+					</span>
+					<span class="topup-tile">
+						<img src="../images/trading_app/cashier/ic_arrow_normal.svg" width="96" height="96" />
+						<p>&nbsp;</p>
+					</span>
+					<span class="topup-tile">
+						<img src="../images/trading_app/cashier/ic_demo_normal.svg" width="96" height="96" />
+						<p>VRTC1234567</p>
+					</span>
+				</div>
+				<div>
+					<Button text="VIEW UPDATED STATEMENT" className="orange secondary" />
+                                	<Button text="TRADE SMART TRADER NOW" className="orange primary" />
+                            	</div>
                         </div>);
 	} 
 }
