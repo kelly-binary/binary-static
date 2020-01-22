c11f232b3831b73347429263a1a8e6dba7c06623
Kelly Anyi <kelly@binary.com>
2020-01-22 13:06:57 +0800

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -35056,6 +35056,9 @@ var ViewPopup = function () {
                 containerSetText('trade_details_entry_spot > span', is_sold_before_start ? '-' : contract.entry_spot_display_value);
             }
             containerSetText('trade_details_message', contract.validation_error && !is_multiplier_contract ? contract.validation_error : '&nbsp;');
+            if (is_multiplier_contract) {
+                containerSetText('trade_details_bottom', localize('This contract is only available on DTrader.[_1][_2]Go to Dtrader[_3] to close or cancel this contract.', ['<br/>', '<a href="https://deriv.app" target="_blank" rel="noopener noreferrer">', '</a>']));
+            }
         }
 
         var is_digit = /digit/i.test(contract.contract_type);
